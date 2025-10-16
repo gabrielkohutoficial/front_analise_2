560fcdd6cfa0a678ebac94028fb52030b84853aa
gabrielkohutoficial <gabrielkohut11@gmail.com>
2025-10-16 00:35:51 +0000

    criação do javascript

---
+++ aulas/javascript/arrays01.js
@@ -0,0 +1,17 @@
+/*
+arrays sao agrupadores de valores numeriscos,testuarios,boleanos,objetos,etc
+eles sao ordenados a partir do indeve'0'
+ex.: const time = [' pedro' , 22, 'julio' , 'ana'] 
+   // indices          0       1     2        3 
+
+*/
+const vestuario = ['camiseta' , 'tenis nike' , 'calça jeans' , 'relogio' , 'chapeu' , 'celular']
+vestuario[33] = ' bone'
+
+for(let valor in vestuario) {
+    console.log(valor)
+}
+// console.log(vestuario[1])
+// console.log(vestuario[0])
+// console.log(vestuario[2])
+// console.log(vestuario[33])
---
+++ aulas/javascript/variaveis.js
@@ -0,0 +1,17 @@
+// alert('ok')
+// console.log('meu nome nao e jonh')
+
+let nome = prompt("qual o seu nome? ")
+let sobrenome = prompt("qual o seu sobrenome? ")
+const cpf = prompt("qual o seu cpf [somente numeros]? ")
+const solteiro = true
+let roupa = prompt("qual é sua roupa? ")
+
+console.log("----PERFIL------\n ")
+console.log("nome: " + nome)
+console.log("sobrenome: " + sobrenome)
+console.log("cpf:" + cpf)
+console.log("solteiro: " + solteiro)
+console.log("roupa: " + roupa)
+console.log("-------------\n ")
+ console.log(nome+sobrenome) // o sinal+ é usado para concatenar valores
